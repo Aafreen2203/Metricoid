c09185b242e9acf497780a8692efe84f29478f5b
Aafreen2203 <aafreen22k@gmail.com>
2025-03-24 16:58:18 +0530

    final commit

--- metri/vite.config.js
+++ metri/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/Aafreen2203/Metricoid.git",
   plugins: [ tailwindcss(),react()],
 })
